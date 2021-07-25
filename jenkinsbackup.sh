e143101114fc28ceb587005f89abcea5dc71eac2
root <root@ip-172-31-8-93.us-east-2.compute.internal>
2021-07-25 10:17:56 +0000

    jenkins backup

--- jenkinsbackup.sh
+++ jenkinsbackup.sh
@@ -7,14 +7,15 @@ git init
 fi
 
 touch .gitignore
-echo workspace >> .gitignore
+echo plugins  >> .gitignore
 
 git add *
 git commit -m "jenkins backup"
 
 git push https://github.com/Sheikrizwan/Jenkinsbackup.git master
 if [ $? -eq 0 ];then
-
+Sheikrizwan
+Rizwan@13
 sendmail -s "sucss" abc@gmail.com
 else
 sendmail -s "fail" abc@gmail.com
